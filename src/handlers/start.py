ac6c951a2d01583e02a9c372043e89c1718d7575
Alexander Zhmurkov <alexander.zhmurkov@gmail.com>
2025-04-22 14:46:48 +0300

    feat: move local file transcription to separate handler

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,12 +1,9 @@
-from aiogram import Bot, F, Router
-from aiogram.filters import CommandStart, StateFilter
+from aiogram import Router
+from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
-from aiogram.types import FSInputFile, Message
+from aiogram.types import Message
 
-from src.config.config import settings
-from src.services.transcription import TranscriptService
 from src.messages.start_msg import MESSAGES
-from src.utils.states import FSMTranscriptionStage
 
 start_router = Router()
 
@@ -14,65 +11,4 @@ start_router = Router()
 @start_router.message(CommandStart())
 async def transcribe_audio(message: Message, state: FSMContext):
     await state.set_state(state=None)
-    await message.answer(text=MESSAGES.get('select_file'))
-    await state.set_state(FSMTranscriptionStage.transcription_run)
-
-
-@start_router.message(
-    F.video | F.audio | F.document,
-    StateFilter(FSMTranscriptionStage.transcription_run)
-)
-async def handle_document(message: Message, bot: Bot, state: FSMContext):
-    await state.set_state(state=None)
-
-    try:
-        if message.audio:
-            file_id = message.audio.file_id
-            original_filename = message.audio.file_name
-            file_size = message.audio.file_size
-        elif message.video:
-            file_id = message.video.file_id
-            original_filename = message.video.file_name
-            file_size = message.video.file_size
-        elif message.document:
-            file_id = message.document.file_id
-            original_filename = message.document.file_name
-            file_size = message.document.file_size
-        else:
-            await message.answer(text=MESSAGES.get('wrong_file_type'))
-            return
-
-        if file_size > 1024 * 1024 * 1024:
-            await message.answer(text=MESSAGES.get('file_too_big'))
-            return
-
-        await message.answer(text=MESSAGES.get('transcription_process'))
-        file = await bot.get_file(file_id)
-        file_path = file.file_path
-        service = TranscriptService(
-            api_key=settings.DEEPGRAM_API_KEY, file_name=file_path
-        )
-        service.run_process()
-        await message.answer(
-            text=MESSAGES.get('transcription_process_completed')
-        )
-
-        document = FSInputFile(
-            path=f'{file_path[:-4]}.txt',
-            filename=f'{original_filename[:-4]}.txt'
-        )
-        await bot.send_document(
-            chat_id=message.chat.id,
-            document=document,
-        )
-
-    except Exception as e:
-        await message.answer(
-            text=MESSAGES.get('common_error').format(e)
-        )
-
-    try:
-        service.clean_garbage()  # noqa
-
-    except Exception:  # noqa
-        pass
+    await message.answer(text=MESSAGES.get('hello'))
